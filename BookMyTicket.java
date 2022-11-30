f001d76550a77aee54509b81593b91d139e9f65b
GeethaBadisi <57406415+GeethaBadisi@users.noreply.github.com>
2022-11-30 09:15:59 +0000

    Add files via upload

---
+++ BookMyTicket.java
@@ -0,0 +1,137 @@
+package com.mindtree.FSE;
+
+public class BookMyTicket {
+    
+    private int movieId;
+    private int noOfTickets;
+    private double discount;
+    private double totalAmount;
+    
+    private static double discountPrice;
+    
+    public int getMovieId() {
+        return movieId;
+    }
+
+
+
+   public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+
+
+   public int getNoOfTickets() {
+        return noOfTickets;
+    }
+
+
+
+    public void setNoOfTickets(int noOfTickets) {
+        this.noOfTickets = noOfTickets;
+    }
+
+
+
+   public double getDiscount() {
+        return discount;
+    }
+
+
+   public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+   public double getTotalAmount() {
+        return totalAmount;
+    }
+
+   public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+    
+    public BookMyTicket(int movieId, int noOfTickets) {
+        super();
+        this.movieId = movieId;
+        this.noOfTickets = noOfTickets;
+    }
+    
+    public double calculateDiscount() {
+        if((movieId==101 || movieId ==102  || movieId== 103) && noOfTickets <5) {
+            return 0;
+        }
+        
+        else if ((movieId ==101 || movieId ==103) && (noOfTickets >=5 && noOfTickets<10)){
+            return 15;
+            
+        }
+        else if ((movieId ==101 || movieId ==103) && (noOfTickets >=10 && noOfTickets<=15)){
+            return 20;
+            
+        }
+        
+        else if (movieId ==102 && (noOfTickets >=5 && noOfTickets<10)){
+            return 15;
+            
+        }
+        
+        else if (movieId ==102 && (noOfTickets >=10 && noOfTickets<=15)){
+            return 15;
+            
+        }
+        
+        else {
+            
+            return 0;
+            
+            }
+        
+            }
+        
+   
+    public double calculateTicketAmount() {
+        
+        if(movieId == 101) {
+            
+            
+            double amount =((120*noOfTickets) -(120*noOfTickets*discountPrice/100));
+            return amount;
+        }
+        
+        else if(movieId == 102) {
+            
+            
+            double amount =((170*noOfTickets) -(170*noOfTickets*discountPrice/100));
+            return amount;
+        }
+        else if(movieId == 103) {
+    
+           
+           double amount =((150*noOfTickets) -(150*noOfTickets*discountPrice/100));
+           return amount;
+        }
+        
+        
+        return 0;
+        
+         
+        
+    }
+    
+   public static void main(String[] args) {
+        
+	   BookMyTicket bookmymovie=new BookMyTicket(101,5);
+        discountPrice=bookmymovie.calculateDiscount();
+        
+        double amount=bookmymovie.calculateTicketAmount();
+        
+        if(amount ==0) {
+            System.out.println("please enter valid movieid");
+        }
+        else {
+            System.out.println("total amount of booking :"+ amount);
+        }
+        
+   }
+}
+//}
